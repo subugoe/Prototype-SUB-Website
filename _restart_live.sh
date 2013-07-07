8d66a207e018b47f0214cc7a720daded72653bd7
Nils Windisch <mail@nilswindisch.de>
2013-07-07 21:18:09 +0200

    revamp jekyll, compass, forman and serve locally file stucture and content
    please note: lastest (2013-07-07) compass, jekyll and foreman gems
    required!

---
+++ _restart_live.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -dr /Users/nw/dev/Prototype-SUB-website/_site
+rm -dr /Users/nw/dev/Prototype-SUB-Website/css
+#rm /Users/nw/dev/Prototype-SUB-Website/css/styles.css
+foreman start -d /Users/nw/dev/Prototype-SUB-Website/ -f _config_foreman_live.procfile
--- _restart_nw.sh
+++ _restart_nw.sh
@@ -2,4 +2,4 @@
 rm -dr /Users/nw/dev/Prototype-SUB-website/_site
 rm -dr /Users/nw/dev/Prototype-SUB-Website/css
 #rm /Users/nw/dev/Prototype-SUB-Website/css/styles.css
-foreman start -d /Users/nw/dev/Prototype-SUB-Website/
+foreman start -d /Users/nw/dev/Prototype-SUB-Website/ -f _config_foreman_dev.procfile
