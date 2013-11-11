cbe80a6a715b08a8161d070f44684f97777b3c32
Nils Windisch <mail@nilswindisch.de>
2013-11-11 13:19:07 +0100

    use sass with sourcemap

--- _config_compass_dev.rb
+++ _config_compass_dev.rb
@@ -6,7 +6,7 @@ css_dir = "css"
 sass_dir = "_css"
 images_dir = "img"
 javascripts_dir = "js"
-sass_options = {:debug_info => true, :cache => false}
+#sass_options = {:debug_info => true, :cache => false, :sourcemap => true}
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
