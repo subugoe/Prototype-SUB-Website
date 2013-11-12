754783f0e658e47e42b0465e130c69ee4d9f0e09
Nils Windisch <mail@nilswindisch.de>
2013-11-12 10:41:55 +0100

    make helper scripts user agnostic

--- _restart_nw.sh
+++ _restart_nw.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm -dr /Users/nw/dev/Prototype-SUB-website/_site
-rm -dr /Users/nw/dev/Prototype-SUB-Website/css
-#rm /Users/nw/dev/Prototype-SUB-Website/css/styles.css
-foreman start -d /Users/nw/dev/Prototype-SUB-Website/ -f _config_foreman_dev.procfile
+rm -dr ~/dev/Prototype-SUB-website/_site
+rm -dr ~/dev/Prototype-SUB-Website/css
+foreman start -d ~/dev/Prototype-SUB-Website/ -f _config_foreman_dev.procfile
